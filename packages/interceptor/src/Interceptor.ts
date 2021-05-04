cb52b5954992513b6167f2d68b2b9919c3716767
Thomas Trainset <gotoanything@foxmail.com>
2021-05-04 21:27:58 +0800

    feat: interceptor

--- packages/interceptor/src/Interceptor.ts
+++ packages/interceptor/src/Interceptor.ts
@@ -1,5 +1,5 @@
 type SuccessHandler<T> = (res: T) => T
-type ErrorHandler<N> = (err: N) => void
+type ErrorHandler<N> = (err: N) => any
 
 interface Handles<T, N> {
   successHandler: SuccessHandler<T>
@@ -9,12 +9,12 @@ interface Handles<T, N> {
 export class Interceptor<T, N> {
   protected handles: Handles<T, N>[] = []
 
-  use(successHandler: SuccessHandler<T>, errorHandler: ErrorHandler<N>) {
+  use(successHandler: SuccessHandler<T>, errorHandler?: ErrorHandler<N>) {
     this.handles.push({ successHandler, errorHandler })
     return this
   }
 
   exec(params: T): Promise<void | T> {
-    return this.handles.reduce((t, c, idx) => t.then(c.successHandler, this.handles[idx - 1]?.errorHandler as any), Promise.resolve(params)).catch(this.handles[this.handles.length - 1].errorHandler)
+    return this.handles.reduce((t, c, idx) => t.then(c.successHandler, this.handles[idx - 1]?.errorHandler), Promise.resolve(params)).catch(this.handles[this.handles.length - 1].errorHandler)
   }
 }
--- packages/interceptor/src/index.ts
+++ packages/interceptor/src/index.ts
@@ -1,6 +1,8 @@
 import { MiddlewareCallback } from '@prequest/types'
 import { Interceptor } from './Interceptor'
 
+export * from './Interceptor'
+
 export class InterceptorMiddleware<T, N, E> {
   request = new Interceptor<T, E>()
   response = new Interceptor<N, E>()
