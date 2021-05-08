34e0b1b795d2b4dcb43b30867532f46b79793d61
Thomas Trainset <gotoanything@foxmail.com>
2021-05-08 19:30:27 +0800

    fix: travel middleware

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,10 +3,10 @@ import { METHODS } from './constant'
 import { merge } from '@prequest/utils'
 import { Context, Config, MethodsCallback, RequestOption, Adapter } from '@prequest/types'
 
-export class PreQuest<T, N> extends Middleware<T, N> {
+export class PreQuestBase<T, N> extends Middleware<T, N> {
   constructor(private adapter: Adapter<T, N>, private config?: Config<T>) {
     super()
-    this.config = merge(PreQuest.defaults, this.config)
+    this.config = merge(PreQuestBase.defaults, this.config)
     this.mount()
   }
 
@@ -30,24 +30,17 @@ export class PreQuest<T, N> extends Middleware<T, N> {
   }
 
   private controller(ctx: Context<T, N>): Promise<N> {
-    return new Promise((resolve, reject) => {
-      return this.exec(ctx, async ctx => {
-        try {
-          const response = await this.adapter(ctx.request)
-          ctx.response = response
-          resolve(response)
-        } catch (e) {
-          reject(e)
-        }
-      })
-    })
+    return this.exec(ctx, async ctx => {
+      const response = await this.adapter(ctx.request)
+      ctx.response = response
+    }).then(() => ctx.response)
   }
 
   static defaults: any = {}
 
   static createInstance<T, N>(adapter: Adapter<T, N>, config?: Config<T>) {
-    return new PreQuest<T, N>(adapter, config) as PreQuestInstance<T, N>
+    return new PreQuestBase<T, N>(adapter, config) as PreQuestBaseInstance<T, N>
   }
 }
 
-export type PreQuestInstance<T, N> = PreQuest<T, N> & MethodsCallback<T, N>
+export type PreQuestBaseInstance<T, N> = PreQuestBase<T, N> & MethodsCallback<T, N>
