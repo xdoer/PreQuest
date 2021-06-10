36ef9ce3ab220e45d3750e9d7b7b3d15b371dc83
Thomas Trainset <gotoanything@foxmail.com>
2021-06-10 20:10:51 +0800

    feat(graphql): req options

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -2,9 +2,10 @@ import { PreQuestInstance } from '@prequest/core'
 import { CommonObject } from '@prequest/types'
 import { merge } from '@prequest/utils'
 
-export function graphql<T, N>(instance: PreQuestInstance<T, N>) {
+export function graphql<T, N>(instance: PreQuestInstance<T, N>, reqOpt?: T) {
   return (query: string, variables?: CommonObject, opt?: T) => {
-    const { data, ...rest } = (opt || {}) as any
+    const options = merge(opt, reqOpt)
+    const { data, ...rest } = (options || {}) as any
     const finalData = merge({ query, variables }, data)
 
     return instance.request({
