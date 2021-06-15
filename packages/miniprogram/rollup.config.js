2c4b21f4aeba4082d088f9fddba1931dda623f50
Thomas Trainset <gotoanything@foxmail.com>
2021-06-15 21:32:45 +0800

    style: rollup config

--- packages/miniprogram/rollup.config.js
+++ packages/miniprogram/rollup.config.js
@@ -1,8 +1,18 @@
 import typescript from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.ts', // 打包入口
-  plugins: [typescript(), terser()],
-  output: { exports: 'auto', file: 'dist/index.js', format: 'esm' },
+  plugins: [
+    nodeResolve(),
+    typescript(),
+    getBabelOutputPlugin({ presets: ['@babel/preset-env'] }),
+    terser(),
+  ],
+  output: [
+    { exports: 'auto', file: 'dist/index.js', format: 'cjs' },
+    { exports: 'auto', file: 'dist/index.ems.js', format: 'es' },
+  ],
 }
