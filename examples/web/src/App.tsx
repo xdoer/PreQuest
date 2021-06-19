61fbbce9c8b508f7e045bc12de14842bd5aef090
Thomas Trainset <gotoanything@foxmail.com>
2021-06-19 17:39:01 +0800

    feat: uploader demo

--- examples/web/src/App.tsx
+++ examples/web/src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { XhrComponent } from './Xhr'
+import { UploadComponent } from './Upload'
 
 function App() {
 
   return (
     <div className="App">
-      <XhrComponent />
+      <UploadComponent />
     </div>
   )
 }
---
+++ examples/web/src/Upload.tsx
@@ -0,0 +1,26 @@
+import React, { FC, useEffect, useState } from 'react'
+import prequest, { Request, Response } from '@prequest/xhr'
+import Uploader from '@prequest/uploader'
+
+
+const uploader = new Uploader({
+  request(formData) {
+    return prequest.post('/upload', {
+      baseURL: 'http://localhost:8080',
+      data: formData
+    })
+  }
+})
+
+export const UploadComponent: FC<{}> = ({ }) => {
+  return (
+    <div>
+      <div>文件上传</div>
+      <input type="file" name="file" onChange={function (e) {
+        const files: any = e.target.files
+        uploader.upload(files[0])
+      }} />
+    </div>
+  )
+}
+
