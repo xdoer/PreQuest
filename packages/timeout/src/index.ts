0d0f8081fe11b329314f008db02879ff5bd8ddde
xdoer <gotoanything@foxmail.com>
2021-06-23 23:01:17 +0800

    feat: support timeoutControl

--- packages/timeout/src/index.ts
+++ packages/timeout/src/index.ts
@@ -1,15 +1,19 @@
 import { MiddlewareCallback } from '@prequest/types'
 import { createError, ErrorCode } from '@prequest/helper'
 
-export interface TimeoutInject {
+export interface TimeoutInject<T> {
   timeout?: number
+  timeoutControl?(opt: T): boolean
 }
 
 export default class TimeoutMiddleware<T, N> {
-  constructor(private opt?: TimeoutInject) {}
+  constructor(private opt?: TimeoutInject<T>) {}
 
-  run: MiddlewareCallback<T & TimeoutInject, N> = async (ctx, next) => {
+  run: MiddlewareCallback<T & TimeoutInject<T>, N> = async (ctx, next) => {
     const timeout = ctx.request.timeout ?? (this.opt?.timeout || 0)
+
+    if (!this.opt?.timeoutControl?.(ctx.request)) return next()
+
     if (timeout <= 0) return next()
 
     await Promise.race([timeoutThrow(timeout, ctx.request), next()])
