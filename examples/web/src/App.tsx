dd6cd3e6ff83a85584995bee59b854d1d7ea6c8c
Thomas Trainset <gotoanything@foxmail.com>
2021-06-28 19:33:07 +0800

    feat(fetch): download example

--- examples/web/src/App.tsx
+++ examples/web/src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { XhrComponent } from './Xhr'
 import { UploadComponent } from './Upload'
+import { FetchComponent } from './Fetch'
 
 function App() {
 
   return (
     <div className="App">
-      <UploadComponent />
+      <FetchComponent />
     </div>
   )
 }
--- examples/web/src/Fetch.tsx
+++ examples/web/src/Fetch.tsx
@@ -4,7 +4,14 @@ import prequest, { Request, Response } from '@prequest/fetch'
 export const FetchComponent: FC<{}> = ({ }) => {
 
   useEffect(() => {
-    prequest.create()
+    prequest('https://vodkgeyttp9c.vod.126.net/vodkgeyttp8/LPz7whmr_2809103936_uhd.mp4?ts=1890099466&rid=7859AE92117216E8AD0E345795682128&rl=0&rs=mqcBSVVeGKqfoCTzNGcRVicCgzugnYvd&sign=7811271dbfc6526c0425f698704b7203&coverId=OxmMzhObT-pnAEa-tsAJhQ==/109951164511633534&infoId=134007', {
+      onDownloadProgress({ loaded, total }) {
+        console.log('查看值', (loaded / total))
+      },
+      responseType: 'blob'
+    }).then(res => {
+      console.log('查看值', res)
+    })
   }, [])
 
   return (
