f98ee40f2dabdfe5d575ca3cd6173f1112b6179f
xdoer <gotoanything@foxmail.com>
2022-04-19 00:27:48 +0800

    refactor(fetch): cancel request

--- packages/fetch/src/adapter.ts
+++ packages/fetch/src/adapter.ts
@@ -7,7 +7,7 @@ import {
 } from '@prequest/helper'
 import { parseResBody } from './helper'
 
-export function adapter(options: Config): Promise<PQResponse> {
+export async function adapter(options: Config): Promise<PQResponse> {
   const finalOptions = (options || {}) as PQRequest
   const url = createRequestUrl(finalOptions!)
   const { data, headers } = formatRequestBodyAndHeaders(finalOptions)
@@ -15,45 +15,41 @@ export function adapter(options: Config): Promise<PQResponse> {
 
   const config = { ...rest, body: data, headers } as any
 
-  if (cancelToken) {
-    cancelToken.promise.then(cancel => {
-      // 环境不支持 abortController，则直接抛异常
-      if (!cancelToken.abortController) {
-        throw createError(ErrorCode.abort, cancel, config)
-      } else {
-        cancelToken.abortController.abort()
-      }
+  async function main() {
+    const fetchResponse = await fetch(url, config)
+    if (!onDownloadProgress || !fetchResponse.body) return parseResBody(fetchResponse, options)
+
+    // 参考: https://segmentfault.com/a/1190000021367378
+    const total = Number.parseInt(fetchResponse.headers.get('content-length') || '')
+    const progress = { total: Number.isNaN(total) ? 0 : total, loaded: 0 }
+    const [progressStream, returnStream] = fetchResponse.body.tee()
+    const reader = progressStream.getReader()
+    const push = async () => {
+      const { value, done } = await reader.read()
+      if (done || !value) return
+      progress.loaded += value.length
+      onDownloadProgress(progress)
+      await push()
+    }
+    await push()
+
+    const newResponse = await new Response(returnStream, {
+      headers: fetchResponse.headers,
+      status: fetchResponse.status,
+      statusText: fetchResponse.statusText,
     })
-    config.signal = cancelToken.abortController?.signal
+
+    return parseResBody(newResponse, options)
   }
 
-  return fetch(url, config)
-    .then(res => {
-      // 如果不需要监听下载，则直接返回
-      if (!onDownloadProgress) return res
-
-      // 如果不存在响应体，则返回
-      if (!res.body) return res
-
-      // 参考: https://segmentfault.com/a/1190000021367378
-      const total = Number.parseInt(res.headers.get('content-length') || '')
-      const progress = { total: Number.isNaN(total) ? 0 : total, loaded: 0 }
-      const [progressStream, returnStream] = res.body.tee()
-      const reader = progressStream.getReader()
-      const push = () => {
-        reader.read().then(({ value, done }) => {
-          if (done || !value) return
-          progress.loaded += value.length
-          onDownloadProgress(progress)
-          push()
-        })
-      }
-      push()
-      return new Response(returnStream, {
-        headers: res.headers,
-        status: res.status,
-        statusText: res.statusText,
+  return Promise.race([
+    main(),
+    cancelToken?.promise
+      .then(cancel => {
+        throw createError(ErrorCode.abort, cancel, config)
       })
-    })
-    .then(res => parseResBody(res, options))
+      .catch(e => {
+        throw e
+      }),
+  ])
 }
