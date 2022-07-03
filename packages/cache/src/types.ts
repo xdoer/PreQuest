1a73f3d33d45967ba5babd24466fd211d9791b29
xdoer <gotoanything@foxmail.com>
2022-07-03 17:54:04 +0800

    feat(cache): type

--- packages/cache/src/types.ts
+++ packages/cache/src/types.ts
@@ -1,10 +1,10 @@
-import { PQRequest } from '@prequest/types'
+import { PQRequest, PQResponse } from '@prequest/types'
 
 export interface Options {
   ttl?: number
   getCacheKey?: (opt: PQRequest) => string
   verifyRequest?: (opt: PQRequest) => boolean
-  verifyResponse?: (response: any) => any
+  verifyResponse?: (response: PQResponse) => any
 }
 
 declare module '@prequest/types' {
