a542845d3a9f6528edd71a588dea13c47659a46c
lzlv312 <lzlv312@qq.com>
2022-07-12 16:56:43 +0800

    some optimize for xhr, fetch, cache

--- packages/cache/src/index.ts
+++ packages/cache/src/index.ts
@@ -1,43 +1,58 @@
-import { merge, AsyncPromise, fallback } from '@xdoer/x'
-import { PQRequest, Adapter, Config } from '@prequest/types'
+import { merge, AsyncPromise } from '@xdoer/x'
+import { PQRequest, Adapter, Config, PQResponse } from '@prequest/types'
 import { Options } from './types'
 
 const defaultOptions: Options = {
   ttl: 1000 * 60,
   getCacheKey: (opt: PQRequest) => opt.path,
   verifyRequest: (opt: PQRequest) => !opt.method || opt.method === 'GET',
-  verifyResponse: fallback,
+  verifyResponse: (res: PQResponse) => !!res,
 }
 
-export default function(options?: Options) {
-  const { getCacheKey, ttl, verifyRequest, verifyResponse } = merge<Required<Options>>(
+export default function (options?: Options) {
+  const { ttl, getCacheKey, verifyRequest, verifyResponse } = merge<Required<Options>>(
     defaultOptions,
     options
   )
-  return function(core: Adapter): Adapter {
+  return function (core: Adapter): Adapter {
     const caches = new Map<string, { promise: Promise<any>; time: number }>()
-    return async function(opt: Config) {
+    let lastTime: number = 0 // last request time
+    return async function (opt: Config) {
       const { useCache = false } = opt
       const _opt = opt as PQRequest
 
       if (useCache && verifyRequest(_opt)) {
         const cacheKey = getCacheKey!(_opt)
-        const cache = caches.get(cacheKey)
+        let cache = caches.get(cacheKey)
+        let nowTime: number = Date.now()
 
-        // if cache is valid
-        if (cache && Date.now() - cache.time < ttl) return cache.promise
+        if (cache && nowTime - cache.time < ttl) return cache.promise // if cache is valid
+        if (lastTime && nowTime - lastTime > ttl) caches.clear()
 
         const promise = new AsyncPromise()
 
-        caches.set(cacheKey, { time: Date.now(), promise: promise.promise })
+        caches.set(cacheKey, { time: nowTime, promise: promise.promise })
+        lastTime = nowTime
 
         try {
-          const value = await verifyResponse(await core(opt))
-          promise.resolve(value)
-          return value
-        } catch (e) {
-          promise.reject(e)
-          throw e
+          const result = await core(opt) // resolve valid cache promise
+          cache = caches.get(cacheKey)
+          if (cache) {
+            if (verifyResponse(result)) {
+              nowTime = Date.now()
+              cache.time = nowTime
+              lastTime = nowTime
+            } else {
+              caches.delete(cacheKey)
+            }
+          }
+          promise.resolve(result)
+          return result
+        } catch (error) {
+          if (caches.has(cacheKey)) {
+            caches.delete(cacheKey)
+          }
+          throw error
         }
       }
 
--- packages/helper/src/index.ts
+++ packages/helper/src/index.ts
@@ -1,5 +1,5 @@
 import { stringify } from 'qs'
-import { isAbsoluteURL, isEmpty } from '@xdoer/x'
+import { is, merge, isAbsoluteURL, isEmpty } from '@xdoer/x'
 import { PresetOption, Common, PQRequest } from '@prequest/types'
 
 export * from './error'
@@ -30,4 +30,34 @@ export function createRequestUrl<
 
 export function requestId(options: PQRequest): string {
   return createRequestUrl({ ...options, params: {} })
+}
+
+export function formatRequestBodyAndHeaders(opt: PQRequest) {
+  const options: any = opt
+  const headers: Common = {}
+  let data = options.data
+
+  const bodyType = is(data)
+
+  let hasContentType: boolean = bodyType === 'formdata'
+  for (let [key, value] of Object.entries(options.headers)) {
+    const _value = value as string
+    if (/^Content-Type$/i.test(key)) {
+      if (hasContentType) break
+      hasContentType = true
+      if (data !== void 0 && bodyType !== 'string') {
+        if (/^application\/x-www-form-urlencoded/i.test(_value)) {
+          data = stringify(data)
+        } else if (/^application\/json/i.test(_value)) {
+          data = JSON.stringify(data)
+        }
+      }
+    }
+  }
+  if (!hasContentType && data !== void 0 && bodyType !== 'string') {
+    headers['Content-Type'] = 'application/json'
+    data = JSON.stringify(data)
+  } // 默认为 json 类型
+
+  return { data, headers: merge<Common>(headers, options.headers) }
 }
